492823ddd79f9b1e9c4fd5bd7656dd01c9408124
SBrahe <201807173@post.au.dk>
2020-04-23 21:06:48 +0200

    Setup added to IT1

--- Microwave.Test.IntegrationV2/IT1_CookController.cs
+++ Microwave.Test.IntegrationV2/IT1_CookController.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Boundary;
 
 namespace Microwave.Test.IntegrationV2
 {
     [TestFixture]
     public class IT1_CookController
     {
-        [Test]
+        private IUserInterface userInterface;
+        private ITimer timer;
+        private IPowerTube powerTube;
+        private IDisplay display;
+        private IOutput output;
+        private ICookController uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            userInterface = Substitute.For<IUserInterface>();
+            output = Substitute.For<IOutput>();
+
+            timer = new Timer();
+            powerTube = new PowerTube(output);
+            display = new Display(output);
+            uut = new CookController(timer,display,powerTube,userInterface);
+        }
 
 
     }
