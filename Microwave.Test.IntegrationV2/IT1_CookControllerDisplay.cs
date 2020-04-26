119be4a00f09c104ced73ed435d346dbb592ecc0
laukvalitet <201807157@post.au.dk>
2020-04-26 20:02:12 +0200

    initial file creations for integration tests

---
+++ Microwave.Test.IntegrationV2/IT1_CookControllerDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Microwave.Test.IntegrationV2
+{
+    [TestFixture]
+    class IT1_CookControllerDisplay
+    {
+        [Test]
+        public void 
+    }
+}
