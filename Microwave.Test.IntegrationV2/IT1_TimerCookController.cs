7b18c89bfe4b22beedf9cc60fdffeefb114b2fcc
Ermin Obradovac <au592117@uni.au.dk>
2020-04-26 20:01:51 +0200

    Hi

---
+++ Microwave.Test.IntegrationV2/IT1_TimerCookController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Boundary;
+
+namespace Microwave.Test.IntegrationV2
+{
+    [TestFixture]
+    public class IT1_TimerCookController
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+    }
+}
